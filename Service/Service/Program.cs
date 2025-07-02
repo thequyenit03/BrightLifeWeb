0c40e7c6399fddb1dc5dfd241d9fbf34205a937f
Do2k3 <nguyenvando020521dta@gmail.com>
2025-07-02 23:28:13 +0700

    LoginModelByDo

--- Service/Service/Program.cs
+++ Service/Service/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 
 namespace Service
 {
@@ -8,11 +9,13 @@ namespace Service
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<Models.ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings")));
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
 
             var app = builder.Build();
 
